0e60e9ceaa4f529687c55e266a60212237f6e413
begin256 <mec29011@163.com>
2019-03-29 22:59:11 +0800

    ws ECHO message

--- ws/ws1.js
+++ ws/ws1.js
@@ -21,3 +21,29 @@ wss.on('connection', function (ws) {
         });
     })
 });
+
+console.log('ws server started at port 3000...');
+
+// client test:
+
+let count = 0;
+
+let ws = new WebSocket('ws://localhost:3000/ws/chat');
+
+ws.on('open', function () {
+    console.log(`[CLIENT] open()`);
+    ws.send('Hello!');
+});
+
+ws.on('message', function (message) {
+    console.log(`[CLIENT] Received: ${message}`);
+    count++;
+    if (count > 3) {
+        ws.send('Goodbye!');
+        ws.close();
+    } else {
+        setTimeout(() => {
+            ws.send(`Hello, I'm Mr No.${count}!`);
+        }, 1000);
+    }
+});
