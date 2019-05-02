221b3f2fea40c92ebb5e729bd085f0c0ca589092
begin256 <mec29011@163.com>
2019-05-02 21:56:33 +0800

    node conn mysql CRUD is ok

--- cai/mysql/test.js
+++ cai/mysql/test.js
@@ -2,13 +2,86 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'test',
-  database : 'test2'
+  password : '123456',
+  database : 'testtest'
 });
  console.log(connection)
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+
+//connect mysql
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+
+//query
+// var  sql = 'SELECT * FROM runoob_tb1';
+// //查
+// connection.query(sql,function (err, result) {
+//         if(err){
+//           console.log('[SELECT ERROR] - ',err.message);
+//           return;
+//         }
+ 
+//        console.log('--------------------------SELECT----------------------------');
+//        console.log(result);
+//        console.log('------------------------------------------------------------\n\n');  
+// });
+ 
+// connection.end();
+
+
+//insert
+// var  addSql = 'INSERT INTO runoob_tb1(runoob_title,runoob_author,submission_date) VALUES (?,?,?)';
+// var  addSqlParams = ['learn node','node',new Date()]
+// //增
+// connection.query(addSql,addSqlParams,function (err, result) {
+//         if(err){
+//          console.log('[INSERT ERROR] - ',err.message);
+//          return;
+//         }        
+ 
+//        console.log('--------------------------INSERT----------------------------');
+//        //console.log('INSERT ID:',result.insertId);        
+//        console.log('INSERT ID:',result);        
+//        console.log('-----------------------------------------------------------------\n\n');  
+// });
+ 
+// connection.end();
+
+
+//update
+// var modSql = 'UPDATE runoob_tb1 SET  runoob_title= ?,runoob_author = ? WHERE runoob_id = ?';
+// var modSqlParams = ['learn node update', 'node update',3];
+// //改
+// connection.query(modSql,modSqlParams,function (err, result) {
+//    if(err){
+//          console.log('[UPDATE ERROR] - ',err.message);
+//          return;
+//    }  
+//    console.log(result)      
+//   console.log('--------------------------UPDATE----------------------------');
+//   console.log('UPDATE affectedRows',result.affectedRows);
+//   console.log('-----------------------------------------------------------------\n\n');
+// });
+ 
+// connection.end();
+
+
+//delete
+var delSql = 'DELETE FROM runoob_tb1 where runoob_id=2';
+//删
+connection.query(delSql,function (err, result) {
+        if(err){
+          console.log('[DELETE ERROR] - ',err.message);
+          return;
+        }        
+ 
+       console.log('--------------------------DELETE----------------------------');
+       console.log('DELETE affectedRows',result.affectedRows);
+       console.log('-----------------------------------------------------------------\n\n');  
 });
+ 
+connection.end();
